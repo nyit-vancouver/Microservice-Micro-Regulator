0aa5b92f41a9989459df18d7b3ea6f2e0a073a8b
Bizhan Beiragh Dar <beyraghdar.bijan@gmail.com>
2023-03-06 22:14:04 -0800

    Add project files.

---
+++ MicroArcSec.Client/Data/YamlSample.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroArcSec.Client.Data
+{
+    public class YamlSample
+    {
+        public string DatabaseConnectionString { get; set; }
+        public string UploadFolder { get; set; }
+        public List<string> ApprovedFileTypes { get; set; }
+    }
+}
